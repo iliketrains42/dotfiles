87636dec11cf7a06592c9e822adc18ae0362452d
unknown <hemanthmedahal@gmail.com>
2025-10-26 23:22:41 +0530

    edited config

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -7,11 +7,35 @@ return {
 
     config = function()
         local lint = require("lint")
+
+        -- Define luacheck custom configuration
+        lint.linters.luacheck = {
+            cmd = "luacheck",
+            stdin = true,
+            args = {
+                "--globals",
+                "vim",
+                "lvim",
+                "reload",
+                "--",
+            },
+            stream = "stdout",
+            ignore_exitcode = true,
+            parser = require("lint.parser").from_errorformat("%f:%l:%c: %m", {
+                source = "luacheck",
+            }),
+        }
+
+        -- Filetype-to-linter mapping
         lint.linters_by_ft = {
             javascript = { "eslint_d" },
             python = { "pylint" },
             cpp = { "cpplint" },
+            lua = { "luacheck" },
         }
+
+        -- Autocommand for linting on certain events
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
             callback = function()
